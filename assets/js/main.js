5991c6767301eacb62730963f63c9ec8757aeebd
mtavidal <119874263+mtavidal@users.noreply.github.com>
2022-12-13 22:39:22 -0300

    Adiciona tela de detalhes do Pokemon na Pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,16 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const pokeDetail = document.getElementById('poke-detail')
+const pokemonArrayList = new Map()
 
 const maxRecords = 151
 const limit = 10
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
+    pokemonArrayList.set(pokemon.number, pokemon)
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li id="poke-${pokemon.number}" class="pokemon ${pokemon.type}" onclick="showDetails('${pokemon.number}')">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -44,4 +47,39 @@ loadMoreButton.addEventListener('click', () => {
     } else {
         loadPokemonItens(offset, limit)
     }
+})
+
+
+function showDetails(pokemonNumber) {
+    const pokemonDetails = pokemonArrayList.get(parseInt(pokemonNumber))
+    const newHtml = detailHtml(pokemonDetails)
+    pokeDetail.className = pokemonDetails.type + ' pokemon'
+    pokeDetail.innerHTML = newHtml
+    document.getElementsByClassName('content')[0].style.display = 'none'
+    document.getElementsByClassName('content-details')[0].style.display = 'block'
+}
+
+function detailHtml(pokemon) {
+    return `
+        <span class="number-detail"> #${pokemon.number} </span>
+        <h1 class="name"> ${pokemon.name} </h1>
+        <div class="detail"><ol class="types"> ${pokemon.types.map((type) => `<li class="type-detail ${type}">${type}</li>`).join('')}</ol></div>
+        <img src="${pokemon.photo}" alt="${pokemon.name}">
+        
+        <ol class="details">
+        
+            <li> Species: ${pokemon.species}</li>
+            <li> Weight: ${pokemon.weight}</li>
+            <li> Height: ${pokemon.height}</li>
+            <li> Abilities: ${pokemon.abilities}</li>
+        
+        
+        </ol>
+        
+    `
+}
+
+backBtn.addEventListener('click', () => {
+    document.getElementsByClassName('content')[0].style.display = 'block'
+    document.getElementsByClassName('content-details')[0].style.display = 'none'
 })
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -5,6 +5,10 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
+    pokemon.abilities = pokeDetail.abilities.map((abilitySlot) => abilitySlot.ability.name)
+    pokemon.height = pokeDetail.height
+    pokemon.weight = pokeDetail.weight
+    pokemon.species = pokeDetail.species.name
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
